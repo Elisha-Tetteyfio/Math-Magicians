e0daf034b30886d4e51c21c0622695fbc5e43c7c
Elisha Tetteyfio <93135004+Elisha-Tetteyfio@users.noreply.github.com>
2022-07-13 23:52:52 +0000

    Add different pages to site

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
-import Calculatorr from './components/Calculator';
+import { Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
+import CalcPage from './pages/CalcPage';
+import Quote from './pages/Quote';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <Calculatorr />
-    </div>
+    <>
+      <Header />
+      <div className="App">
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/quote" exact>
+            <Quote />
+          </Route>
+          <Route path="/calculator" exact>
+            <CalcPage />
+          </Route>
+        </Switch>
+      </div>
+    </>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import LoremIpsum from 'react-lorem-ipsum';
+
+const Home = () => (
+  <>
+    <h2>Welcome to our page!</h2>
+    <div className="text1">
+      <LoremIpsum />
+    </div>
+    <div className="text1">
+      <LoremIpsum />
+    </div>
+  </>
+);
+
+export default Home;
