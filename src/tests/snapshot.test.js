de05e2685bedca9b08bedb0fa2fe61465d32bf4f
Bwalya Nonde <bwalyanonde8@gmail.com>
2022-07-15 21:37:21 -0400

    Update snapshot.test file

--- src/tests/snapshot.test.js
+++ src/tests/snapshot.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import { StaticRouter } from 'react-router-dom';
 import Header from '../components/Header';
-// import * as styles from '../components/Header.css';
 
 it('Test header', () => {
   const tree = renderer
